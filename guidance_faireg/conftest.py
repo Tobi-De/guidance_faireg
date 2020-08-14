6ae4d075861afd7f955160aaada2bb9b79edeeb1
Tobi DEGNON <tobidegnon@protonmail.com>
2020-08-14 11:01:22 +0100

    first commit

---
+++ guidance_faireg/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from guidance_faireg.users.models import User
+from guidance_faireg.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
